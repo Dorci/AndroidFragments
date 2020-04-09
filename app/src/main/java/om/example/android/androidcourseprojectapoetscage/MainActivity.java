052e5ce633fdb0fb962a0983fd2e73370f009a4d
Dorci <43410253+Dorci@users.noreply.github.com>
2020-04-09 11:27:54 +0200

    second commit

---
+++ app/src/main/java/om/example/android/androidcourseprojectapoetscage/MainActivity.java
@@ -0,0 +1,14 @@
+package om.example.android.androidcourseprojectapoetscage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
